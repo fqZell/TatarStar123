fec835efe7f710439d2024007833da4e4bf6c367
fqZell <mulkyway00@gmail.com>
2025-03-16 13:54:43 +0300

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,36 @@
 const init = () => {
     headerModal()
     swiper()
+    headerBcc()
 }
 
 const headerModal = () => {
 
-    const headerBurger = document.querySelector(".header-modal__open")
-    if (!headerBurger) return false
+    const headerBurger = document.querySelectorAll(".header-modal__icons")
 
     const headerModalEl = document.querySelector(".header-modal")
-    if (!headerModal) return false
 
     const overlayEl = document.querySelector('.overlay')
-    if (!overlayEl) return false
 
-    // const headerClose = document.querySelector(".header-modal__close")
-    // if (!headerClose) return false
+    const headerClose = document.querySelector(".header-modal__icons.close")
 
-    headerBurger.addEventListener("click", () => {
-        headerModalEl.classList.toggle("active")
-        overlayEl.classList.toggle("active")
-        // headerClose.classList.toggle("active")
-    })
+    const headerOpen = document.querySelector(".header-modal__icons.open")
+
+    const bodyEl = document.querySelector("body")
+
+    if (!headerBurger || !headerModalEl || !overlayEl || !headerClose || !headerOpen || !bodyEl) return false
+
+
+    headerBurger.forEach(burger => {
+        burger.addEventListener("click", () => {
+            headerModalEl.classList.toggle("active")
+            overlayEl.classList.toggle("active")
+            headerClose.classList.toggle("active")
+            headerOpen.classList.toggle("active")
+            bodyEl.classList.toggle("active")
+        })
+    });
+    
 
 }
 
@@ -42,6 +51,24 @@ const swiper = ()  => {
         //   prevEl: ".swiper-button-prev",
         // },
     });
+}
+
+const headerBcc = () => {
+
+    const headerEl = document.querySelector("header")
+    
+    if (!headerEl) return false
+
+    window.addEventListener("scroll", () => {
+
+        if (window.scrollY > window.innerHeight/2) {
+            headerEl.style.backgroundColor = "#828282"
+        } else {
+            headerEl.style.backgroundColor = "transparent"
+        }
+
+    })
+
 }
 
 document.addEventListener("DOMContentLoaded", init);
